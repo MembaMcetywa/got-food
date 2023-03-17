73e9bf9137c653da1561dec80acf8302c0b73094
MembaMcetywa <iamemba10@gmail.com>
2023-03-17 14:52:57 +0200

    feature(Checkout): added client side validation

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -34,7 +34,7 @@ const Checkout = (props) => {
     setIsFormInputValid({
       name: nameIsValid,
       street: streetIsValid,
-      postal: enteredPostal,
+      postalCode: enteredPostal,
       city: cityIsValid,
     });
 
@@ -48,22 +48,40 @@ const Checkout = (props) => {
 
   return (
     <form className={classes.form} onSubmit={confirmHandler}>
-      <div className={classes.control}>
+      <div
+        className={`${classes.control} ${
+          isFormInputValid.name ? "" : classes.invalid
+        }`}
+      >
         <label htmlFor="name">Your Name</label>
         <input ref={nameInputRef} type="text" id="name" />
         {!isFormInputValid.name && <p>Please enter a name</p>}
       </div>
-      <div className={classes.control}>
+      <div
+        className={`${classes.control} ${
+          isFormInputValid.street ? "" : classes.invalid
+        }`}
+      >
         <label htmlFor="street">Street</label>
         <input ref={streetInputRef} type="text" id="street" />
         {!isFormInputValid.street && <p>Please enter a street</p>}
       </div>
-      <div className={classes.control}>
+      <div
+        className={`${classes.control} ${
+          isFormInputValid.postalCode ? "" : classes.invalid
+        }`}
+      >
         <label htmlFor="postal">Postal Code</label>
         <input ref={postalInputRef} type="text" id="postal" />
-        {!isFormInputValid.postalCode && <p>Please enter a city</p>}
+        {!isFormInputValid.postalCode && (
+          <p>Please enter a valid postal code.</p>
+        )}
       </div>
-      <div className={classes.control}>
+      <div
+        className={`${classes.control} ${
+          isFormInputValid.city ? "" : classes.invalid
+        }`}
+      >
         <label htmlFor="city">City</label>
         <input ref={cityInputRef} type="text" id="city" />
         {!isFormInputValid.city && <p>Please enter a city</p>}
